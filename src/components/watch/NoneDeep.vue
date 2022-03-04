244b9a97cf1a66c7a173ed61a97af23aad368bac
malodegachi <malo.degachi@bosa.fgov.be>
2022-03-04 12:32:11 +0100

    DEeep <--> none deep

---
+++ src/components/watch/NoneDeep.vue
@@ -0,0 +1,39 @@
+<template>
+  <input v-model="deep.text" />
+  <h4> address </h4>
+  <input v-model="deep.address.street" />
+  <input v-model="deep.address.number" />
+  <input v-model="deep.address.city" />
+</template>
+
+<script>
+export default {
+  name: "NoneDeep",
+  props: {
+    modelValue: Object
+  },
+  data() {
+    return {
+      deep: {
+        text: '',
+        address: {
+          street: null,
+          number: null,
+          city: null
+        }
+      }
+    }
+  },
+  watch: {
+    deep: {
+      handler(value) {
+        this.$emit('update:modelValue', value)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/WatchView.vue
+++ src/views/WatchView.vue
@@ -1,3 +1,43 @@
 <template>
   <h1>Watch</h1>
+  <br/>
+  <br/>
+
+  <h3> Deep </h3>
+  <br/>
+
+  <Deep v-model="deep"/>
+  <p>
+    Deep value: {{deep}}
+  </p>
+
+  <br/>
+  <br/>
+
+  <h3> None deep </h3>
+  <br/>
+  <NoneDeep v-model="noneDeep"/>
+  <p>
+    None deep value: {{noneDeep}}
+  </p>
 </template>
+
+<script>
+import Deep from './../components/watch/Deep.vue'
+import NoneDeep from './../components/watch/NoneDeep.vue'
+
+export default {
+  name: 'WatchView',
+  components: {
+    Deep,
+    NoneDeep
+  },
+  data() {
+    return {
+      deep: null,
+      noneDeep: null
+    }
+  }
+};
+
+</script>
