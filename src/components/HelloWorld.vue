7f4b16c2181cec2a9528f9454162044cf3c88523
djuju08 <julien_nart@yepmail.net>
2022-03-04 11:32:51 +0100

    add views and route for each topic

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,9 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <RouterLink to="/">
+      <h1 class="green">{{ msg }}</h1>
+    </RouterLink>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,49 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import FormValidation from '../views/FormValidation.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import FormValidation from "../views/FormValidation.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import("../views/AboutView.vue"),
     },
     {
-      path: '/form-validation',
-      name: 'form validation',
-      component: FormValidation
-    }
-  ]
-})
+      path: "/form-validation",
+      name: "form validation",
+      component: FormValidation,
+    },
+    {
+      path: "/v-model",
+      name: " vmodel",
+      component: () => import("../views/VModelView.vue"),
+    },
+    {
+      path: "/mutate-prop",
+      name: " mutateProp",
+      component: () => import("../views/MutatePropView.vue"),
+    },
+    {
+      path: "/watch",
+      name: " watch",
+      component: () => import("../views/WatchView.vue"),
+    },
+    {
+      path: "/v-for",
+      name: " vfor",
+      component: () => import("../views/VForView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
