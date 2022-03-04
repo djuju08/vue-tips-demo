8e0c58a3648a90f981606afbc3a9d186c66adb0b
djuju08 <julien_nart@yepmail.net>
2022-03-04 08:56:20 +0100

    fix import

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 import HelloWorld from "@/components/HelloWorld.vue";
 </script>
 
@@ -15,11 +15,6 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
     <div class="wrapper">
       <HelloWorld msg="Vue tips" />
-
-      <!-- <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav> -->
     </div>
   </header>
 
